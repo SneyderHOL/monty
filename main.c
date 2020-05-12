f63bafcd78a33fdf1ffb251178d3c6a3519b0231
SneyderHOL <1507@holbertonschool.com>
2020-05-12 11:40:34 -0500

    Add .m  and .gitignore files

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: a char pointer to the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv)
+{
+	if (argc != 2)
+	{
+		write(stderr, "USAGE: monty file");
+	}
+
+}
