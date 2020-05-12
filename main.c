732c5c45151e885b02413e276c658f2ad4504394
SneyderHOL <1507@holbertonschool.com>
2020-05-12 18:34:58 -0500

    [UPDATE]: add missing code in main

--- main.c
+++ main.c
@@ -8,10 +8,34 @@
  */
 int main(int argc, char *argv)
 {
+	char *filename = NULL, *line = NULL, *args[] = {NULL, NULL, NULL};
+	FILE *fptr = NULL;
+	size_t lineSize = 0;
+	ssize_t line_read_char = 0;
+	int count = 0, i = 0;
+
 	if (argc != 2)
 	{
 		write(STDERR_FILENO, "USAGE: monty file\n", 18);
 		return (EXIT_FAILURE);
 	}
-
+	filename = argv[1];
+	validate_filename(filename);
+	fptr = fopen(filename, "r");
+	validate_file(fptr);
+	line_read_char = getline(&line, &lineSize, fptr);
+	for (count = 1; line_read_char != -1; count++)
+	{
+		split_line(args, line);
+		for (i = 0; i < 3 && args[i] != NULL; i++)
+		{
+			printf("%s\n", args[i]);
+		}
+		line_read_char = getline(&line, &lineSize, fptr);
+		/*printf("%zu\n", line_read_char);
+		  printf("%s\n", line);*/
+	}
+	free(line);
+	fclose(fptr);
+	return (0);
 }
